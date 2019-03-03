ca944ab5891f7183e28600d138daebdbc8fa0984
Andrey Glazachev <cnitsa@shell.sf.net>
2019-03-03 20:18:35 +0700

    [*] minor change: imports

--- resources/java/net/sf/timeslottracker/gui/reports/ReportConfiguration.java
+++ resources/java/net/sf/timeslottracker/gui/reports/ReportConfiguration.java
@@ -9,7 +9,6 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.WindowEvent;
 import java.io.BufferedWriter;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
